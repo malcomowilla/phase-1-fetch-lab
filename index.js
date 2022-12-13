f88087f69e3ff026d905956ad37cfe3fb6d05cb4
malcomowilla <owillamalcom@gmail.com>
2022-12-13 08:35:04 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+    fetch("https://anapioficeandfire.com/api/books")
+  .then(response=>response.json())
+  .then(renderBooksData=>renderBooks(renderBooksData))
 }
+fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,6 +15,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+renderBooks()
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
